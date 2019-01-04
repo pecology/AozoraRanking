dfd9847b55b5d8c30f4dce2d0ffc157654b63efc
pecology <kobayashireo877@gmail.com>
2019-01-05 02:44:50 +0900

    ajaxでデータを取得する処理テスト実装

--- top.js
+++ top.js
@@ -1,3 +1,11 @@
 $(function(){
-  $('body').text("こんにちは！！！");
+  $.ajax({
+    type: 'GET',
+    url: 'resources/rankings.json.gz',
+    dataType: 'json'  
+  }).done((data) => {
+    const a =1;
+    const b = 3;
+    console.log(data);
+  });
 });
