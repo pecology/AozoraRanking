16152c082e1400905a41fbd247646f06cb070ea0
pecology <kobayashireo877@gmail.com>
2019-01-06 04:06:05 +0900

    グラフを見れるようにした

--- top.js
+++ top.js
@@ -1,11 +1,39 @@
+let monthRankings;
+
 $(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'resources/rankings.json',
-    dataType: 'json'  
-  }).done((data) => {
-    const a =1;
-    const b = 3;
-    console.log(data);
-  });
+  google.charts.load('current', {'packages':['corechart']});
+
+  google.charts.setOnLoadCallback(initialize);
+
+  function initialize() {
+    $.get('resources/rankings.json').done((result) => {
+      monthRankings = result;
+
+      const currentMonthRankings = monthRankings[monthRankings.length - 1].rankings;
+      const currentMonth = monthRankings[monthRankings.length - 1].month;
+
+      const data =  new google.visualization.DataTable();
+      data.addColumn('string', 'rankAndTitle');
+      data.addColumn('number', 'pageview');
+
+      const slisedRankings = currentMonthRankings.slice(0, 10);
+      slisedRankings.forEach(element => {
+        data.addRow([`${element.rank}.${element.title}`, element.pageview]);
+      });
+      
+      const options = {'title':`${currentMonth} access ranking`,
+                       'width': 960,
+                       'height': 700,
+                       hAxis: {
+                        slantedText: true,
+                        slantedTextAngle: -90
+                       }
+                      };
+
+      const chart = new google.visualization.ColumnChart(document.getElementById('chart'));
+      chart.draw(data, options);
+    });
+  };
+
+  
 });
